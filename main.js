9605361da58342c39c911c8e28fe6882e39bb1f2
イワムラシオン <68310617+shion212@users.noreply.github.com>
2021-03-01 23:17:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -23,15 +23,3 @@ $(function() {                       //run when the DOM is ready
 });
 
 
-//ハンバーガー
-$(document).ready(function () {
-
-        $('.menu').click(function() {
-            $('.overlay').toggleClass('anim');
-            $(this).addClass('open')
-        });
-
-        $('.open').click(function(){
-            $('.overlay').toggleClass('reverse-animation');
-        })
-});
